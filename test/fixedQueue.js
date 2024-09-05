7ad3f5ca73884e433207d6e82024e64cfcadc778
Timur Sevimli <svmlitimur@gmail.com>
2024-09-05 22:35:56 +0300

    Add tests for unshift method

--- test/fixedQueue.js
+++ test/fixedQueue.js
@@ -3,7 +3,7 @@
 const { test, plan } = require('tap');
 const { FixedQueue } = require('../collections.js');
 
-plan(11);
+plan(13);
 
 test('FixedQueue initialization', (t) => {
   t.plan(2);
@@ -35,9 +35,38 @@ test('push method (large queue)', (t) => {
 
   for (let i = 0; i < MAX; i++) {
     queue.push(i);
+test('unshift method (small queue)', (t) => {
+  t.plan(6);
+
+  const queue = new FixedQueue();
+
+  queue.unshift(1);
+  queue.unshift(2);
+  queue.unshift(3);
+
+  t.equal(queue.shift(), 3);
+  t.equal(queue.length, 2);
+  t.equal(queue.shift(), 2);
+  t.equal(queue.length, 1);
+  t.equal(queue.shift(), 1);
+  t.equal(queue.length, 0);
+});
+
+test('unshift method (large queue)', (t) => {
+  t.plan(2);
+
+  const MAX = 3_000;
+
+  const expected = new Array(MAX).fill(undefined);
+  const queue = new FixedQueue();
+
+  for (let i = 0; i < MAX; i++) {
+    queue.unshift(i);
+    expected[MAX - i - 1] = i;
   }
 
   t.equal(queue.length, MAX);
+  t.same([...queue], expected);
 });
 
 test('shift method (small queue)', (t) => {
